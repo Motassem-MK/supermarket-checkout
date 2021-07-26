5fb2bf266caa25843b3e947b9a3570e349766bd4
Motassem <motassemkassab@teknasyon.com>
2021-07-26 22:18:37 +0300

    Feature: support creating related offers in ProductFactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Offer;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -26,4 +27,16 @@ class ProductFactory extends Factory
             'price' => $this->faker->randomFloat(2, 1, 500)
         ];
     }
+
+    public function withOffer(string $type, array $parameters)
+    {
+        return $this->afterCreating(
+            fn(Product $product) => Offer::create([
+                'product_id' => $product->id,
+                'type' => $type,
+                'parameters' => $parameters
+            ])
+        );
+    }
+
 }
